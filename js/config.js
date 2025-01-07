064ce2978c5e372dc5efb7b211198017c3fe6f9e
lance4444 <lancewithoutlot@outlook.com>
2025-01-07 13:05:49 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_qVpIesYfA', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7d46q86bn02upt94pu64ibsuje',// e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        userPoolClientSecret:'1gndseltbbga4b29d7d2fl8so51c3dk6nvo08k42n4juegnh0vdh',
+        userPoolId: 'us-east-1_jW1PM5zdH', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4gqup151utnkcvm3ggfrv4a0ko',// e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
